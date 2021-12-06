2398aad9f5c71a514f5226bc0dca4564878a9928
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-06 14:00:49 +0700

    Update userbot.py

--- MusicAndVideo/userbot.py
+++ MusicAndVideo/userbot.py
@@ -154,6 +154,7 @@ async def repo(client, m: Message):
 
 ❤️‍🔥 KREDIT DEVOLOPER
 • [Kyy](https://github.com/muhammadrizky16)
-• [Kyy](https://github.com/zxcskyy)</b>
+• [Kyy](https://github.com/zxcskyy)
+• [Tomi](https://github.com/XtomiSN</b>
 """
     await m.reply(REPO, disable_web_page_preview=True)
